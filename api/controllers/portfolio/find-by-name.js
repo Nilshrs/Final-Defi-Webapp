8ced5736de01765daade6529cd9acbdc8e84f34f
Nilshrs <nilsherhaus@gmail.com>
2022-12-14 19:31:59 +0100

    feat: search bar with automatically filter option for types

---
+++ api/controllers/portfolio/find-by-name.js
@@ -0,0 +1,46 @@
+module.exports = {
+
+
+  friendlyName: 'Find by name',
+
+
+  description: 'Find token by Name',
+
+
+  inputs: {
+
+    name: {type: 'string', required: true}
+
+  },
+
+
+  exits: {
+
+    success: {
+      viewTemplatePath: 'pages/portfolio/trans02'
+    },
+    redirect: {
+      responseType: 'redirect',
+      description: 'if token is not of search type then redirect to previous page'
+    }
+  },
+
+
+  fn: async function (inputs) {
+
+    console.log("Trying to find token by name for portfolio " + inputs.name);
+
+    const type = this.req.session.trans.type;
+
+    // All done.
+    const tokens = await Token.find( { name: { startsWith: inputs.name }, type } )
+
+
+    if(tokens.length === 0){
+      throw { redirect: '/trans-01/' + type }
+    }
+
+    return { tokens };
+
+  }
+};
